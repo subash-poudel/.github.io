ef87fbe6c18ff5705419a70c980ecdd7deb16e3a
Subash Poudel <subash@smartmobe.com>
2019-11-28 13:32:51 +0545

    Updates

---
+++ precache-manifest.da6861f63435ebd5259e8d2d7ad2fef3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ff71b578562b334998214d640c90c64",
+    "url": "/subash-poudel.github.io/index.html"
+  },
+  {
+    "revision": "6f8cb3aae9cf00380a29",
+    "url": "/subash-poudel.github.io/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "1d888b311d535e458897",
+    "url": "/subash-poudel.github.io/static/js/2.2bceaec4.chunk.js"
+  },
+  {
+    "revision": "6f8cb3aae9cf00380a29",
+    "url": "/subash-poudel.github.io/static/js/main.405da237.chunk.js"
+  },
+  {
+    "revision": "b7b0a2e68f4442cf411d",
+    "url": "/subash-poudel.github.io/static/js/runtime-main.31aca757.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/subash-poudel.github.io/static/media/logo.25bf045c.svg"
+  }
+]);
